398900bceec8e4214bed783dcb5b3387bde8d651
JuanVillanueva <jvillanuevae@unal.edu.co>
2023-02-11 17:05:09 -0500

    primera sesion

---
+++ trabajo_propio/00_hola_mundo.py
@@ -0,0 +1,12 @@
+#Esto es un comentario:
+#Mi primer hola mundo como programador 
+print ("Hola python")
+
+"""Aqui tbm se pueden dejar comentarios
+en varias lineas como ayuda 
+auqnue queda rojo"""
+
+#consultar tipo de dato type
+print (type("hola mundo")) #tipo str (cadena de texto)
+print (type (54)) #tipo int (numerico entero)
+print (type (1.5)) #tipo float (no enteros)
