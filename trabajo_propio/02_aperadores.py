2bafda85044cf043bf20cbcab4ca332155581c16
JuanVillanueva <jvillanuevae@unal.edu.co>
2023-02-11 21:16:09 -0500

    segunda sesion

---
+++ trabajo_propio/02_aperadores.py
@@ -0,0 +1,33 @@
+print (3+4) # suma 
+print (3-4) # resta 
+print (3*4) # multiplicacion 
+print (3/4) # division 
+print (10%5) # moduelo (residuo de la division)
+print (10//3) # resultado de la division entero 
+print (2**3) # exponentes   
+
+
+# comparadores 
+
+print (3 < 4 )  # menor que 
+print (3 > 4 ) # mayor que 
+print (3 <= 4 ) # menor o igual que 
+print (3 >= 4 ) #mayor o igual que 
+print (3 == 4 ) # igual que 
+print (3 != 4 ) # diferente  
+
+# ordenacion alfabetica, < = antes y > = despues 
+
+print ("a" < "b") 
+print ("a" <= "b")
+print ("a" > "b")
+print ("a" >= "b")
+print ("a" == "b")
+print ("a" != "b")
+
+# operadores logicos, algebra boleana 
+
+print (3 > 4 ) and (3 > 4 ) # falso porque son dos falsos 
+print (3 <= 4 ) or (3 >= 4 ) # verdadero porquer hay un true 
+print (not(3 != 4 )) # niega al true 
+print (not(3 == 4 )) # niega el false 
---
+++ trabajo_propio/03_string.py
@@ -0,0 +1,21 @@
+#salto de linea /n 
+string_con_salto = "hola como estas"
+print  (string_con_salto) 
+# no los coge 
+
+name, surname, age = "Juan", "David", 18 
+
+print ("mi nombre es {} {} y tengo {} años".format (name, surname, age)) 
+print (f"mi nombre es {name} {surname} y tengo {age} años")
+
+print (f"hola {name} {surname}") 
+
+# desempaquetado de caracteres 
+laguage = "python"
+a, b, c, d, e, f = laguage 
+print (a)
+print (b) 
+print (c) 
+print (d)
+print (e)
+print (f) 
